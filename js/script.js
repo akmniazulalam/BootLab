e4b300c39ef4042825f6676963624a539b48c801
niazulalam <niazulalam097@gmail.com>
2025-03-01 17:56:03 +0600

    Venobox Added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'maxWidth': 300,
+})
+
+
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane',
+});
